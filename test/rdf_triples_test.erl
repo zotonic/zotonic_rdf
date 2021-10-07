af345b46e03e7b3d152560f703656beb08901c01
Marc Worrell <marc@worrell.nl>
2021-10-07 17:48:12 +0200

    Add test for lang/type attrs.

--- test/rdf_triples_test.erl
+++ test/rdf_triples_test.erl
@@ -33,6 +33,29 @@ to_docs2_test() ->
     ],
     ?assertEqual(Doc, rdf_triples:to_docs(triples2())).
 
+to_docs3_test() ->
+    Doc = [
+        #{
+            <<"@id">> => <<"http://example.com/#a">>,
+            <<"http://xmlns.com/foaf/0.1/name">> => [
+                <<"Jan">>
+            ],
+            <<"http://example.com/#foo">> => [
+                <<"Foo">>,
+                #{
+                    <<"http://example.com/#bar">> => [
+                        #{
+                            <<"@value">> => <<"Bar">>,
+                            <<"@lang">> => <<"nl">>,
+                            <<"@type">> => <<"http://example.com/#string">>
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    ?assertEqual(Doc, rdf_triples:to_docs(triples3())).
+
 
 
 triples1() ->
@@ -65,5 +88,31 @@ triples2() ->
             <<"subject">> => <<"_:n1">>,
             <<"predicate">> => <<"http://example.com/#bar">>,
             <<"@value">> => <<"Bar">>
+        }
+    ].
+
+triples3() ->
+    [
+        #{
+            <<"subject">> => <<"http://example.com/#a">>,
+            <<"predicate">> => <<"http://xmlns.com/foaf/0.1/name">>,
+            <<"@value">> => <<"Jan">>
+        },
+        #{
+            <<"subject">> => <<"http://example.com/#a">>,
+            <<"predicate">> => <<"http://example.com/#foo">>,
+            <<"@value">> => <<"Foo">>
+        },
+        #{
+            <<"subject">> => <<"http://example.com/#a">>,
+            <<"predicate">> => <<"http://example.com/#foo">>,
+            <<"@id">> => <<"_:n1">>
+        },
+        #{
+            <<"subject">> => <<"_:n1">>,
+            <<"predicate">> => <<"http://example.com/#bar">>,
+            <<"@value">> => <<"Bar">>,
+            <<"@lang">> => <<"nl">>,
+            <<"@type">> => <<"http://example.com/#string">>
         }
     ].
